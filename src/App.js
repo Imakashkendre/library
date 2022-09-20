40b2c9cc1600d01c142f096f82bc9ada936c7a9e
Imakashkendre <akash.shopisthan@gmail.com>
2022-09-20 09:52:26 +0530

    Repo create

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Slider from './Components/carousel/Slider';
+import RestoNavbar from './Components/NavBars/NavbarWithDropDown/RestoNavbar';
+import Nav from './Components/NavBars/Navwithhamburger/Nav';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <RestoNavbar /> */}
+      <Nav />
+      {/* <Slider /> */}
+    </>
   );
 }
 
